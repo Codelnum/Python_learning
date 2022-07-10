6e23b1a7108e1126de858b0fb5f12eb91c7d9038
Codelnum <mvpugachev@mail.ru>
2022-07-10 19:31:30 +0300

    1-Homework

---
+++ 1-homework.py
@@ -0,0 +1,35 @@
+#Принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+'''
+day=int(input('введите число от 1 до 7:  '))
+def day_check(a):
+    if (day == 6 or day == 7):
+        print ("it's weekend")
+    elif (day>0 and day<6):
+        print ("it's not a weekend")
+    else:
+        print ('bad day')
+
+day_check(day)
+'''
+#Принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка (или на какой оси она находится).
+x = int(input('введите координату Х:  '))
+y = int(input('введите координату Y:  '))
+if (x == 0 and y ==0):
+    print('это зеро.')
+else:
+    def quarter_check(a,b):
+        if (a>0 and b>0):
+            print('1st quarter')
+        elif (a<0 and b>0):
+            print('2nd quarter')
+        elif (a<0 and b<0):
+            print('3rd quarter')
+        elif ( a>0 and b<0):
+            print('4th quarter')
+        elif (a==0 and b!=0):
+            print('on axis X')
+        elif(a!=0 and b ==0):
+            print('on axis Y')
+        else:
+             print('в астрале')
+quarter_check(x,y)
