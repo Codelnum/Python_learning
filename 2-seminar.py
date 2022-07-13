f6e00a7759ba5a396cd2349c6dd4e5be48995909
Codelnum <mvpugachev@mail.ru>
2022-07-13 23:32:19 +0300

    seminar_2

--- 2-seminar.py
+++ 2-seminar.py
@@ -25,7 +25,7 @@ print(f'distance: {distance}')
 '''
 
 # В институте биоинформатики по офису передвигается робот. Недавно студенты из группы программистов написали для него программу, по которой робот, когда заходит в комнату, считает количество программистов в ней и произносит его вслух: "n программистов". Для того, чтобы это звучало правильно, для каждого nn нужно использовать верное окончание слова. Напишите программу, считывающую с пользовательского ввода целое число nn (неотрицательное), выводящее это число в консоль вместе с правильным образом изменённым словом "программист", для того, чтобы робот мог нормально общаться с людьми, например: 1 программист, 2 программиста, 5 программистов. В комнате может быть очень много программистов. Проверьте, что ваша программа правильно обработает все случаи, как минимум до 1000 человек.
-
+'''
 num_string = input("введите кол-во человек: ") 
 num = int(num_string)
 string = "программист"
@@ -49,3 +49,50 @@ else:
             print(f'{num} {string+end2}')
         else:
             print(f'{num} {string+end1}')
+            
+'''
+#КНБ - вы играете с ботом, варианты раунда игры - победа 1 очко, проигрыш 0 очков, ничья 0.5 балла, чтоб статистика сохранялась, и можно было играть неограниченное количество раундов
+p_score = 0
+b_score = 0
+
+while True:
+    num1 = int(input("введите число (1- камень, 2 - ножницы, 3 - бумага):  "))
+    from random import randint
+    num2 = randint(1, 3)
+    if num1 == 1 and num2 ==1:
+        p_score+=0.5
+        b_score+=0.5
+        print(f"no win. (player:{p_score}, bot:{b_score}")
+    elif num1 == 1 and num2 == 2:
+        p_score+=1
+        print(f"player win (player:{p_score}, bot:{b_score}")
+    elif num1 == 1 and num2 == 3:
+        b_score+=1
+        print("computer win (player:{p_score}, bot:{b_score}")
+    elif num1 == 2 and num2 == 2:
+        p_score+=0.5
+        b_score+=0.5
+        print("no win (player:{p_score}, bot:{b_score}")
+    elif num1 == 2 and num2 == 1:
+        b_score+=1
+        print("computer win (player:{p_score}, bot:{b_score}")
+    elif num1 == 2 and num2 == 3:
+        p_score+=1
+        print(f"player win (player:{p_score}, bot:{b_score}")
+
+    elif num1 == 3 and num2 == 3:
+        p_score+=0.5
+        b_score+=0.5
+        print("no win (player:{p_score}, bot:{b_score}")
+    elif num1 == 3 and num2 == 2:
+        b_score+=1
+        print("computer win (player:{p_score}, bot:{b_score}")
+    elif num1 == 3 and num2 == 1:
+        p_score+=1
+        print(f"player win (player:{p_score}, bot:{b_score}")
+    else:
+        print("введите число от 1 до 3")
+
+    ask = input('еще партию? Введите: +/- ')
+    if ask !="+":
+        break
